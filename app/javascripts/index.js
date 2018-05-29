8b7194abd7a7f9ea7cdf80260d497415e9a8abf9
Masataka Umamichi <masaumamichi@live.jp>
2018-05-29 18:16:03 +0900

    add sub pug

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 
-
 // root React container
 ReactDOM.render(<App />, document.getElementById('root'));
